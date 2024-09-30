dffedc13a19cf6c18fb0ae5e6c608d414807c2a3
jumagu <jmgg.gutierrez@gmail.com>
2024-09-29 19:04:16 -0500

    feat: Add AuthLayout, LoginPage, and RegisterPage components

---
+++ src/modules/auth/pages/LoginPage.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleLogin = () => {
+  localStorage.setItem('token', 'XYZ-123');
+
+  const lastPath = localStorage.getItem('last-path') ?? '/';
+
+  router.replace(lastPath);
+};
+</script>
+
+<template>
+  <h1 class="text-2xl font-semibold mb-4">Login</h1>
+  <form>
+    <!-- Username Input -->
+    <div class="mb-4">
+      <label for="username" class="block text-gray-600">Username</label>
+      <input
+        id="username"
+        type="text"
+        name="username"
+        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        autocomplete="off"
+      />
+    </div>
+    <!-- Password Input -->
+    <div class="mb-4">
+      <label for="password" class="block text-gray-600">Password</label>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        autocomplete="off"
+      />
+    </div>
+    <!-- Remember Me Checkbox -->
+    <div class="mb-4 flex items-center">
+      <input id="remember" type="checkbox" name="remember" class="text-blue-500" />
+      <label for="remember" class="text-gray-600 ml-2 select-none">Remember Me</label>
+    </div>
+    <!-- Forgot Password Link -->
+    <div class="mb-6 text-blue-500">
+      <RouterLink to="" class="hover:underline">Forgot Password?</RouterLink>
+    </div>
+    <!-- Login Button -->
+    <button
+      type="button"
+      class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+      @click="handleLogin"
+    >
+      Login
+    </button>
+  </form>
+  <!-- Sign up  Link -->
+  <div class="mt-6 text-blue-500 text-center">
+    <RouterLink :to="{ name: 'register' }" class="hover:underline">Sign up Here</RouterLink>
+  </div>
+</template>
---
+++ src/modules/auth/pages/RegisterPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1 class="text-2xl font-semibold mb-4">Resgister</h1>
+  <form>
+    <!-- Username Input -->
+    <div class="mb-4">
+      <label for="username" class="block text-gray-600">Username</label>
+      <input
+        id="username"
+        type="text"
+        name="username"
+        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        autocomplete="off"
+      />
+    </div>
+    <!-- email Input -->
+    <div class="mb-4">
+      <label for="email" class="block text-gray-600">Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        autocomplete="off"
+      />
+    </div>
+    <!-- Password Input -->
+    <div class="mb-4">
+      <label for="password" class="block text-gray-600">Password</label>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        autocomplete="off"
+      />
+    </div>
+    <!-- Remember Me Checkbox -->
+    <div class="mb-4 flex items-center">
+      <input id="remember" type="checkbox" name="remember" class="text-blue-500" />
+      <label for="remember" class="text-gray-600 ml-2 select-none">Remember Me</label>
+    </div>
+    <!-- Register Button -->
+    <button
+      type="submit"
+      class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+    >
+      Create Account
+    </button>
+  </form>
+  <!-- Login  Link -->
+  <div class="mt-6 text-blue-500 text-center">
+    <RouterLink :to="{ name: 'login' }" class="hover:underline">Log in Here</RouterLink>
+  </div>
+</template>
