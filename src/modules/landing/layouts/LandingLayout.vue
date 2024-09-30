e17ef6fe386a6c7772c5cc97c4895ef92dd6fabc
jumagu <jmgg.gutierrez@gmail.com>
2024-09-30 12:39:14 -0500

    refactor: Update LandingLayout.vue to improve responsiveness, add pokemon page link with pokeball icon

--- src/modules/landing/layouts/LandingLayout.vue
+++ src/modules/landing/layouts/LandingLayout.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex flex-col h-screen">
-    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
-      <div>
-        <a class="flex items-center gap-2 font-semibold" href="#">
-          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
-        </a>
+    <header
+      class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8 py-4"
+    >
+      <div class="w-10 mr-2">
+        <RouterLink :to="{ name: 'home' }">
+          <img src="@/assets/logo.svg" alt="Vue logo" />
+        </RouterLink>
       </div>
-      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6 overflow-auto">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="features">Features</RouterLink>
         <RouterLink to="pricing">Pricing</RouterLink>
         <RouterLink to="contact">Contact</RouterLink>
         <RouterLink
           :to="{ name: 'login' }"
-          class="text-white bg-blue-500 px-3 py-1 rounded-md hover:bg-blue-600 transition-colors duration-300"
+          class="text-white bg-blue-500 px-3 py-1 rounded-md hover:bg-blue-600 transition-colors duration-300 whitespace-nowrap"
         >
           Log in
         </RouterLink>
@@ -28,10 +30,16 @@
       </RouterView>
     </main>
 
-    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+    <footer
+      class="relative flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8 py-4"
+    >
       <p class="flex-1 text-sm text-gray-500 text-center">
         © {{ new Date().getFullYear() }} XYZ Corporation. All rights reserved
       </p>
+      <RouterLink
+        class="pokeball md:absolute right-4 md:right-6"
+        :to="{ name: 'pokemon', params: { id: 1 } }"
+      />
     </footer>
   </div>
 </template>
